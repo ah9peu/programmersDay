0eee9c797aa1c72909064dd9d98bf9e5e6b18fa0
Andrey.Koss <AndreyVKoss@yandex.ru>
2022-06-22 23:24:25 +0300

    code optimized a bit

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,10 +1,7 @@
 public class LeapYear {
 
     public static boolean isLeapYear (int year) {
-
-        boolean result = !((year % 4 != 0) | ((year % 100 == 0) & (year % 400 != 0)));
-
-        return result;
+        return !((year % 4 != 0) | ((year % 100 == 0) & (year % 400 != 0)));
     }
 
 }
